df842ad1e5f0ca42c901db0718d6474449aa9267
Fujimoto <genfujimoto@gmail.comi>
2021-03-23 22:24:56 +0900

    Update

--- install/nvim_install_ubuntu20.sh
+++ install/nvim_install_ubuntu20.sh
@@ -2,7 +2,8 @@ sudo apt update && upgrade
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt update
 sudo apt install neovim/focal
-
+sudo apt install python3 python3-pip
+python3 -m pip install neovim pynvim
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.vim/dein
 
@@ -22,9 +23,11 @@ cp -f ./configs/vimrc ~/.config/nvim/init.vim
 cp -f ./configs/plugins.toml ~/.vim/dein/
 cp -f ./configs/lazy_plugins.toml ~/.vim/dein/
 
-sudo apt install clang-format lua cmake
+sudo apt install clang-format lua cmake g++-8
+export CXX=g++-8
+export CMAKE_CXX_COMPILER=g++-8
 nvim
 read -p "Hit enter: "
-sudo apt install go node
+sudo apt install golang-go node
 cd ~/.vim/dein/repos/github.com/valloric/youcompleteme
 ./install.py --all
