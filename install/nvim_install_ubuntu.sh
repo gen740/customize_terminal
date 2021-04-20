d43fdf0ce60d63d64b3ca095d26757e4c409d22f
Fujimoto <genfujimoto@gmail.comi>
2021-04-20 21:46:29 +0900

    update

--- install/nvim_install_ubuntu.sh
+++ install/nvim_install_ubuntu.sh
@@ -3,6 +3,9 @@ sudo apt -y install python3 python3-pip
 sudo apt -y install clang-format cmake g++-8
 sudo apt -y install golang-go nodejs npm
 python3 -m pip install neovim pynvim
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
 
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.vim/dein
@@ -18,12 +21,6 @@ if ! [ -e ~/.cofig/nvim/ ]; then
   fi
   mkdir -p ~/.config/nvim/
 fi
-
-cd ~/.config
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
-mv squashfs-root / && ln -s /squashfs-root/AppRun /usr/bin/nvim
 
 cp -f ./configs/vimrc ~/.config/nvim/init.vim
 cp -f ./configs/nvimrc ~/.nvimrc
