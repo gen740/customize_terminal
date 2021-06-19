8e80257016577cd65d3096744f4a46893aac2ae6
20er_Fujimoto(local) <genfujimoto@gmail.com>
2021-06-20 01:24:37 +0900

    Add costom_func.zsh

--- install/configs/update.sh
+++ install/configs/update.sh
@@ -5,4 +5,5 @@ cp -rf ~/.vim/custom_runtime .
 cp -f ~/.zshrc ./zshrc
 cp -f ~/.tmux.conf ./tmux.conf
 cp -rf ./* ../../dotfiles
+cp -f ~/.config/zsh/custom_func.zsh ../../dotfiles
 rm ../../dotfiles/update.sh
