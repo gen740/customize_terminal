6d93b2365855c148f713bd855cd2af80d6d6283a
20er_Fujimoto(local) <genfujimoto@gmail.com>
2021-06-20 00:59:01 +0900

    add .tmux.conf

--- install/configs/update.sh
+++ install/configs/update.sh
@@ -1,6 +1,6 @@
 cp -f ~/.config/nvim/init.vim ./vimrc
-cp -f ~/.nvimrc ./nvimrc
 cp -f ~/.vim/dein/plugins.toml ./plugins.toml
 cp -f ~/.vim/dein/lazy_plugins.toml ./lazy_plugins.toml
 cp -rf ~/.vim/custom_runtime .
 cp -f ~/.zshrc ./zshrc
+cp -f ~/.tmux.conf ./tmux.conf
